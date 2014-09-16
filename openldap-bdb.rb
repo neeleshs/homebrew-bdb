8f96b2f8ec8b6aeff3f2ff57050e9d7c4f9120d8
Neelesh Shastry <neelesh@marketo.com>
2014-09-16 10:53:19 -0700

    BDB-5.1.29 formula

---
+++ openldap-bdb.rb
@@ -0,0 +1,25 @@
+require 'formula'
+
+class Openldap < Formula
+  homepage 'http://www.openldap.org/software/'
+  url 'ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-2.4.39.tgz'
+  sha256 '8267c87347103fef56b783b24877c0feda1063d3cb85d070e503d076584bf8a7'
+
+  depends_on 'bdb-5.1.29' => :optional
+  option 'with-memberof', 'Include memberof overlay'
+  option 'with-sssvlv', 'Enable server side sorting and virtual list view'
+
+  def install
+    args = %W[--disable-dependency-tracking
+              --prefix=#{prefix}
+              --sysconfdir=#{etc}
+              --localstatedir=#{var}]
+    args << "--enable-bdb=no" << "--enable-hdb=no" if build.without? "berkeley-db"
+    args << "--enable-memberof" if build.with? "memberof"
+    args << "--enable-sssvlv=yes" if build.with? "sssvlv"
+
+    system "./configure", *args
+    system "make install"
+    (var+'run').mkpath
+  end
+end
