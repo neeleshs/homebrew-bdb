3f56c2ce87d65844890b3f6904276fa4b985dff3
Neelesh Shastry <neelesh@marketo.com>
2014-09-18 13:18:04 -0700

    OpenLDAP

--- Formula/openldap-bdb.rb
+++ Formula/openldap-bdb.rb
@@ -17,7 +17,7 @@ class OpenldapBdb < Formula
     args << "--enable-bdb=no" << "--enable-hdb=no" if build.without? "bdb-5.1.29"
     args << "--enable-memberof" if build.with? "memberof"
     args << "--enable-sssvlv=yes" if build.with? "sssvlv"
-    system({"LDFLAGS" => "-L/usr/local/Cellar/bdb-5.1.29/5.1.29/lib", "CPPFLAGS" => "-I/usr/local/Cellar/bdb-5.1.29/5.1.29/include", "LD_LIBRARY_PATH" => "/usr/local/Cellar/bdb-5.1.29/5.1.29/lib"}, "./configure "+ args.join(" "))
+    exec({"LDFLAGS" => "-L/usr/local/Cellar/bdb-5.1.29/5.1.29/lib", "CPPFLAGS" => "-I/usr/local/Cellar/bdb-5.1.29/5.1.29/include", "LD_LIBRARY_PATH" => "/usr/local/Cellar/bdb-5.1.29/5.1.29/lib"}, "./configure "+ args.join(" "))
     system "make install"
     (var+'run').mkpath
   end
